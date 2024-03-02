ef92b63dba2e5f8ea54053daf72b1421b93b0f3d
Andrea Esposito <esposito_andrea99@hotmail.com>
2024-03-02 14:48:46 +0100

    Update personal publications

--- update-references.sh
+++ update-references.sh
@@ -4,4 +4,4 @@ bib2bib \
 	-ob "${1:-/dev/stdout}" \
 	--expand --expand-xrefs \
 	-c 'author : "Esposito" & $type <> "UNPUBLISHED"' \
-	"${2:-"$HOME/Documents/Lavoro/Ricerca/Literature/My Library.bib"}"
+	"${2:-"$HOME/Documents/Lavoro/Ricerca/Literature/Library.bib"}"
