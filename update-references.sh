7539628d3a459264b480309698757261464d464d
Andrea Esposito <esposito_andrea99@hotmail.com>
2024-03-19 23:03:33 +0100

    Remove MSc and BSc thesis from publications

--- update-references.sh
+++ update-references.sh
@@ -3,5 +3,5 @@
 bib2bib \
 	-ob "${1:-/dev/stdout}" \
 	--expand --expand-xrefs \
-	-c 'author : "Esposito" & $type <> "UNPUBLISHED"' \
+	-c 'author : "Esposito" & ($type <> "UNPUBLISHED" & $type <> "MASTERSTHESIS" & !($type : "PHDTHESIS" & type : "Bachelor Thesis"))' \
 	"${2:-"$HOME/Documents/Lavoro/Ricerca/Literature/Library.bib"}"
